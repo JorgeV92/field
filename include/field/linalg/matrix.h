814715c21556983bb7cef6624d3b7ec54223f31a
JorgeV92 <jorgevmg31@gmail.com>
2026-03-12 20:20:57 -0500

    more matrix operations drafts

--- include/field/linalg/matrix.h
+++ include/field/linalg/matrix.h
@@ -30,7 +30,30 @@ public:
             data_.insert(data_.end(), r.begin(), r.end());
         }
     }
+    std::size_t Rows() const { return rows_; }
+    std::size_t Cols() const { return cols_; }
+    bool Empty() const { return data_.empty(); }
+    static Matrix Identity(std::size_t n) {Matrix i(n, n, T()); for (std::size_t ii = 0; ii < n; ii++){i(ii,ii) = T(1);} return i;}
+    T& operator()(std::size_t row, std::size_t col) {return data_.at(Index(row, col));}
+    const T& operator()(std::size_t row, std::size_t col) const { return data_.at(Index(row, col)); }
+    Vector<T> Row(std::size_t row) const {
+        std::vector<T> v(cols_);
+        for (std::size_t c = 0; c < cols_; ++c) {
+            v[c] = (*this)(row, c);
+        }
+        return Vector<T>(std::move(v));
+    }
+    Vector<T> Col(std::size_t col) const {
+        std::vector<T> v(rows_);
+        for (std::size_t r = 0; r < rows_; ++r) {
+            v[r] = (*this)(r, col);
+        }
+        return Vector<T>(std::move(v));
+    }
+    Matrix& operator+=(const Matrix& o) {CheckSameShape(o); for (std::size_t i = 0; i < data_.size(); ++i) {data_[i] += o.data_[i];} return *this;}
+    
 private:
+    void CheckSameShape(const Matrix& o) const {if (rows_ != o.rows_ || cols_ != o.cols_) throw std::invalid_argument("Matrix shape mismatch.");}
     std::size_t rows_;
     std::size_t cols_;
     std::vector<T> data_;
